909ccc6f4c80b5f6af98defdf29bb01cc7e8f950
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2023-03-02 01:55:42 -0500

    Update Quete.java

--- Quete.java
+++ Quete.java
@@ -1,33 +1,33 @@
 package ca.udem.ift1025.tp1.corrige.guildcommands;
 
-import ca.udem.ift1025.tp1.corrige.guildcommands.GuildCommand;
-import ca.udem.ift1025.tp1.corrige.guildcommands.GuildCommandSystem;
 
-
-public class Quete extends Guild{
+public class Quete{
 
     private double lifeCost;
     private int cashReward;
     private int armorReward;
 
 
-    public void doQuest(){
 
-        level category=getCategory();
-        double life=
-        setLifePoints(lifeCost)
-        for (int i=0;i<heroList.length;i++){
+
+    //  public void doQuest(){
+
+    //    int category=getCategory();
+
+       // setLifePoints(lifeCost);
+       // for (int i=0;i<heroList.length;i++){
             //TO-DO; Sorting Algo
-        }
+      //  }
 
-        for (int i=0;i<heroList.length;i++){
+      //  for (int i=0;i<heroList.length;i++){
             //TO-DO; Search category
-        }
+      //  }
 
 
 
 
-    }
+
 
 
 }
+
