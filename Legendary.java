44a3a9ae5f27a876c39f035a301e1bf683310494
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2023-03-02 03:38:48 -0500

    Update Legendary.java

--- Legendary.java
+++ Legendary.java
@@ -2,18 +2,10 @@ package ca.udem.ift1025.tp1.corrige.guildcommands;
 
 public class Legendary extends Hero{
 
-    private int category;
 
-    public Legendary(String name, double cashCost, int armorCost, double lifePoints) {
-        super(name, cashCost, armorCost, lifePoints);
-    }
 
-    public void setCategory(int category) {
-        this.category = category;
-    }
-
-    public int getCategory() {
-        return category;
+    public Legendary (String name, double cashCost, int armorCost, double lifePoints, int category) {
+        super(name, cashCost, armorCost, lifePoints, category);
     }
 
 }
