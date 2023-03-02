64c77436cb13dc585afb885a93a0f0c2bbffb29c
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2023-03-02 01:58:16 -0500

    Create Uncommon.java

---
+++ Uncommon.java
@@ -0,0 +1,17 @@
+package ca.udem.ift1025.tp1.corrige.guildcommands;
+
+public class Uncommon extends Hero{
+
+    private int category;
+    public Uncommon(String name, double cashCost, int armorCost, double lifePoints) {
+        super(name, cashCost, armorCost, lifePoints);
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+}
