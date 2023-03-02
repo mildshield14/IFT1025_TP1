77e589d6498d5198ba1b427d60677cdfaad3b33e
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2023-03-02 03:37:39 -0500

    Update Epic.java

--- Epic.java
+++ Epic.java
@@ -1,17 +1,8 @@
 package ca.udem.ift1025.tp1.corrige.guildcommands;
 
 public class Epic extends Hero{
-    private int category;
 
-    public Epic(String name, double cashCost, int armorCost, double lifePoints) {
-        super(name, cashCost, armorCost, lifePoints);
-    }
-
-    public void setCategory(int category) {
-        this.category = category;
-    }
-
-    public int getCategory() {
-        return category;
+    public Epic(String name, double cashCost, int armorCost, double lifePoints, int category) {
+        super(name, cashCost, armorCost, lifePoints, category);
     }
 }
