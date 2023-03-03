56ee7538eaec6463ea935e942e29cbfbf410e359
marionnee <114243727+marionnee@users.noreply.github.com>
2023-03-02 19:11:11 -0500

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -1,12 +1,13 @@
 package ca.udem.ift1025.tp1.corrige.guildcommands;
 
-public class Hero extends Guild{
+public class Hero extends Guild {
 
     private String name;
     private double cashCost;
     private int armorCost;
 
     private double lifePoints;
+    private double maxLifePoints;
 
     int category;
 
@@ -14,14 +15,16 @@ public class Hero extends Guild{
         return category;
     }
 
-    public Hero(String name, double cashCost, int armorCost, double lifePoints, int category ){
+    public Hero(String name, double cashCost, int armorCost, double lifePoints, int category, double maxLifePoints) {
         super();
-        this.name=name;
-        this.armorCost=armorCost;
-        this.cashCost=cashCost;
-        this.lifePoints=lifePoints;
-        this.category=category;
+        this.name = name;
+        this.armorCost = armorCost;
+        this.cashCost = cashCost;
+        this.lifePoints = lifePoints;
+        this.category = category;
+        this.maxLifePoints = maxLifePoints;
     }
+
     public String getName() {
         return name;
     }
@@ -38,14 +41,19 @@ public class Hero extends Guild{
     public int getArmorCost() {
         return armorCost;
     }
-
+    public double getMaxLifePoints() {
+        return maxLifePoints;
+    }
     public void setLifePoints(double lifePoints) {
         this.lifePoints = lifePoints;
 
+    }
+    public void setMaxLifePoints(double maxLifePoints) {
+        this.maxLifePoints = maxLifePoints;
     }
 
     public void setCategory(int category) {
         this.category = category;
     }
-
 }
+
