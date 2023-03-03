60c461397bd66f42238775e6c1b5213857567b93
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2023-03-02 21:01:15 -0500

    Update Rare.java

--- Rare.java
+++ Rare.java
@@ -1,9 +1,9 @@
 package ca.udem.ift1025.tp1.corrige.guildcommands;
 
-public class Uncommon extends Hero{
+public class Rare extends Hero{
 
 
-    public Uncommon(String name, double cashCost, int armorCost, double lifePoints, int category, double maxLifePoints)
+    public Rare(String name, double cashCost, int armorCost, double lifePoints, int category, double maxLifePoints)
     {
         super(name, cashCost, armorCost, lifePoints, category, maxLifePoints);
     }
