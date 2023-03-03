97b9a1c69801e8fd90797cf709b73961e136bf17
marionnee <114243727+marionnee@users.noreply.github.com>
2023-03-02 19:09:29 -0500

    Update Legendary.java

--- Legendary.java
+++ Legendary.java
@@ -3,9 +3,10 @@ package ca.udem.ift1025.tp1.corrige.guildcommands;
 public class Legendary extends Hero{
 
 
-
-    public Legendary (String name, double cashCost, int armorCost, double lifePoints, int category) {
-        super(name, cashCost, armorCost, lifePoints, category);
+    public Legendary(String name, double cashCost, int armorCost, double lifePoints, int category, double maxLifePoints)
+    {
+        super(name, cashCost, armorCost, lifePoints, category, maxLifePoints);
     }
+
 
 }
