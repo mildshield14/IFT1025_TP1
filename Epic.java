80f61967a84c031bad1ff4f4ea3997fa47fedd76
marionnee <114243727+marionnee@users.noreply.github.com>
2023-03-02 19:07:40 -0500

    Update Epic.java

--- Epic.java
+++ Epic.java
@@ -2,7 +2,10 @@ package ca.udem.ift1025.tp1.corrige.guildcommands;
 
 public class Epic extends Hero{
 
-    public Epic(String name, double cashCost, int armorCost, double lifePoints, int category) {
-        super(name, cashCost, armorCost, lifePoints, category);
+
+    public Epic(String name, double cashCost, int armorCost, double lifePoints, int category, double maxLifePoints) {
+        super(name, cashCost, armorCost, lifePoints, category, maxLifePoints);
     }
+
+
 }
