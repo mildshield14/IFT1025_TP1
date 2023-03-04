7041fdb8be6e731fdfc8bc6b9466ee0fbb8faaf7
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2023-03-04 01:45:05 -0500

    Space management

--- Common.java
+++ Common.java
@@ -2,10 +2,9 @@ package ca.udem.ift1025.tp1.corrige.guildcommands;
 
 public class Common extends Hero{
 
-
-    public Common(String name, double cashCost, int armorCost, double lifePoints, int category, double maxLifePoints) {
+    public Common(String name, double cashCost, int armorCost, double lifePoints, int category, double maxLifePoints)
+    {
         super(name, cashCost, armorCost, lifePoints, category, maxLifePoints);
     }
-
 
 }
