b3ba92e5d0c7cbf7a10be3dab9291501e4d8cb32
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2023-03-04 01:45:41 -0500

    Space management

--- Uncommon.java
+++ Uncommon.java
@@ -1,12 +1,11 @@
 package ca.udem.ift1025.tp1.corrige.guildcommands;
 
-public class Uncommon extends Hero{
-
+public class Uncommmon extends Hero{
 
     public Uncommon(String name, double cashCost, int armorCost, double lifePoints, int category, double maxLifePoints)
     {
         super(name, cashCost, armorCost, lifePoints, category, maxLifePoints);
     }
 
-
 }
+
