d6f9d20469efbfe1ec8706290836b721e99e1221
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2023-03-04 01:39:47 -0500

    Space management

--- Legendary.java
+++ Legendary.java
@@ -2,11 +2,9 @@ package ca.udem.ift1025.tp1.corrige.guildcommands;
 
 public class Legendary extends Hero{
 
-
     public Legendary(String name, double cashCost, int armorCost, double lifePoints, int category, double maxLifePoints)
     {
         super(name, cashCost, armorCost, lifePoints, category, maxLifePoints);
     }
-
 
 }
