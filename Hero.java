4b779004ca6763e735eae93586a8afab330a8e48
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2023-03-04 01:44:08 -0500

    better space management and comments

--- Hero.java
+++ Hero.java
@@ -1,14 +1,12 @@
 package ca.udem.ift1025.tp1.corrige.guildcommands;
 
-public class Hero extends Guild {
+public class Hero{
 
     private String name;
     private double cashCost;
     private int armorCost;
-
     private double lifePoints;
     private double maxLifePoints;
-
     int category;
 
     public int getCategory() {
@@ -16,7 +14,7 @@ public class Hero extends Guild {
     }
 
     public Hero(String name, double cashCost, int armorCost, double lifePoints, int category, double maxLifePoints) {
-        super();
+
         this.name = name;
         this.armorCost = armorCost;
         this.cashCost = cashCost;
@@ -25,33 +23,30 @@ public class Hero extends Guild {
         this.maxLifePoints = maxLifePoints;
     }
 
+    //getters
     public String getName() {
         return name;
     }
-
     public double getCashCost() {
         return cashCost;
     }
-
     public double getLifePoints() {
         return lifePoints;
     }
-
-
     public int getArmorCost() {
         return armorCost;
     }
     public double getMaxLifePoints() {
         return maxLifePoints;
     }
+
+    //setters
     public void setLifePoints(double lifePoints) {
         this.lifePoints = lifePoints;
-
     }
     public void setMaxLifePoints(double maxLifePoints) {
         this.maxLifePoints = maxLifePoints;
     }
-
     public void setCategory(int category) {
         this.category = category;
     }
