d8096fdc79390a4c1c6df23cba4153136d4dc4e3
mildshield14 <80240232+mildshield14@users.noreply.github.com>
2023-03-04 01:40:21 -0500

    Space management

--- Rare.java
+++ Rare.java
@@ -2,11 +2,9 @@ package ca.udem.ift1025.tp1.corrige.guildcommands;
 
 public class Rare extends Hero{
 
-
     public Rare(String name, double cashCost, int armorCost, double lifePoints, int category, double maxLifePoints)
     {
         super(name, cashCost, armorCost, lifePoints, category, maxLifePoints);
     }
-
 
 }
